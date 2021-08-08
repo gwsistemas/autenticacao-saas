87542cff4f4eeb8924c9056b7f4e477d4b5e8d2d
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-08 00:27:03 -0300

    refactor: move HttpPostClient and HttpPostClientSpy to its file

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from '../protocols/http/httpPostClient'
+import { HttpPostClient } from '../protocols/http/http-post-client'
 
 export class HttpClientPostSpy implements HttpPostClient {
   url: string
