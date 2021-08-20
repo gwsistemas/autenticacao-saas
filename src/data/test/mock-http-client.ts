5aa4878d72aba2e0dafb559acc8c8fdd2f924094
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-20 09:35:18 -0300

    test: ensure RemoteAuthentication throws Unexpected error on default case

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,10 +1,11 @@
 import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '../protocols/http'
 
+// This class is a mock to HttpAxiosClient, HttpFetchClient...
 export class HttpClientPostSpy implements HttpPostClient {
   url: string
   body?: object
   response: HttpResponse = {
-    statusCode: HttpStatusCode.noContent
+    statusCode: HttpStatusCode.ok
   }
 
   async post (params: HttpPostParams): Promise<HttpResponse> {
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -47,4 +47,20 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+  it('should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+  it('should throw Unexpected if HttPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
