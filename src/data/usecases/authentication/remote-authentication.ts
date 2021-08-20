277ae194b239229e69502a55798e3d4a5245faff
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-19 23:55:43 -0300

    refactor: ensure the auth method send the correct body to api

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,16 +1,18 @@
+import { AuthenticationParamsMapper } from '@/data/mappers'
 import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
-import { InvalideCredentialsError } from '@/domain/errors/invalide-credentials-error'
-import { Authentication } from '@/domain/usercases'
+import { InvalideCredentialsError, UnexpectedError } from '@/domain/errors'
+import { Authentication } from '@/domain/usecases'
 
 export class RemoteAuthentication {
   constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {}
   async auth (params: Authentication.Params): Promise<void> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
-      body: params
+      body: AuthenticationParamsMapper.authParams(params)
     })
     switch (httpResponse.statusCode) {
       case HttpStatusCode.unauthorized: throw new InvalideCredentialsError()
+      case HttpStatusCode.badRequest: throw new UnexpectedError()
     }
   }
 }
