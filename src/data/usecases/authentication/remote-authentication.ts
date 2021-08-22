6893bb1459d0e5c3ebafb7133cb1542bc6c775b2
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-21 23:24:53 -0300

    feat: ensure RemoteAuthentication return accountModel and return statusCode 200

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -2,18 +2,18 @@ import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 import { InvalideCredentialsError, UnexpectedError } from '@/domain/errors'
 import { Authentication } from '@/domain/usecases'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (private readonly url: string,
     private readonly httpPostClient: HttpPostClient<Authentication.Params, Authentication.Model>
   ) {}
 
-  async auth (params: Authentication.Params): Promise<void> {
+  async auth (params: Authentication.Params): Promise<Authentication.Model> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.unauthorized: throw new InvalideCredentialsError()
       default: throw new UnexpectedError()
     }
