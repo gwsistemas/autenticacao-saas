0551971386bafdf618a17b8952d8b4ddbee841b1
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-22 00:32:15 -0300

    feat: ensure AxiosHttpClient return correct body

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,17 @@
-import { HttpPostParams } from '@/data/protocols/http'
+import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async post (params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body)
+/**
+ * Está classe usa Adapter Pattern para fazer a interface
+ * do HttpPostClient e do Axios para não deixar nosso sistema
+ * dependente do axios
+ */
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
