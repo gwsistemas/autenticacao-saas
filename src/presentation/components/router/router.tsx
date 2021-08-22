4e30b4b5a69e21e18296a448b333cec0c3a6db84
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-22 19:28:00 -0300

    feat: add Router component

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Login } from '@/presentation/pages'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Login} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router
