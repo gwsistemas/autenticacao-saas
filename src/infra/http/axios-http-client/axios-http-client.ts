81e08d9a0f5227e415877f37aba83e3a5e6f7d63
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-22 00:10:38 -0300

    feat: ensure AxiosHttpClient receive correct values and verb

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,7 +2,7 @@ import { HttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 
 export class AxiosHttpClient {
-  async post (parmas: HttpPostParams<any>): Promise<void> {
-    await axios.post(parmas.url)
+  async post (params: HttpPostParams<any>): Promise<void> {
+    await axios.post(params.url, params.body)
   }
 }
