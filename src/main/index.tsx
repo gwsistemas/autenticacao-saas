e6369cbdae4d5201fff103fe4d9305ce53ce8173
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-22 19:24:10 -0300

    feat: main layer creation and start the Login screen

---
+++ src/main/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Router } from '@/presentation/components'
+
+ReactDom.render(<Router />, document.getElementById('main'))
---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      LoginScreen 123
+    </div>
+  )
+}
+
+export default Login
