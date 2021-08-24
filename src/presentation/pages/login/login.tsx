6f1e68d64070b300e66b860337cc0c61be1fb5ff
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-24 20:38:58 -0300

    chore: add fast refresh

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Login: React.FC = () => {
   return (
     <div>
-      LoginScreen 123
+      LoginScreen
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const webpack = require('webpack')
+const ReactRefreshTypeScript = require('react-refresh-typescript')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+console.log(isDevelopment)
 
 module.exports = {
   mode: 'development',
@@ -19,7 +26,15 @@ module.exports = {
     rules: [
       {
         test: /\.ts(x?)$/,
-        loader: 'ts-loader',
+        loader: require.resolve('ts-loader'),
+        options: {
+          getCustomTransformers: () => ({
+            before: isDevelopment ? [ReactRefreshTypeScript()] : []
+          }),
+          // `ts-loader` does not work with HMR unless `transpileOnly` is used.
+          // If you need type checking, `ForkTsCheckerWebpackPlugin` is an alternative.
+          transpileOnly: isDevelopment
+        },
         exclude: /node_modules/
       },
       {
@@ -51,5 +66,9 @@ module.exports = {
     react: 'React',
     'react-dom': 'ReactDOM'
   },
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    isDevelopment && new ReactRefreshWebpackPlugin()
+  ].filter(Boolean)
 }
