02bbccc1eedbedb0a92185f6c71149b1edb76f3c
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-29 16:49:25 -0300

    test: ensure Input start correct state

---
+++ src/presentation/components/input/input.spec.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { cleanup, render, RenderResult } from '@/main/config/test-utils'
+import faker from 'faker'
+import { Input } from '@/presentation/components'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const mockedInput = {
+  helpText: faker.name.firstName()
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Input helpText={mockedInput.helpText} disabled />)
+  return {
+    sut
+  }
+}
+
+describe('Input Component', () => {
+  afterEach(cleanup)
+  it('should render helpText', () => {
+    const { sut } = makeSut()
+    const helpeText = sut.getByTestId('help-text')
+    expect(helpeText.textContent).toBe(mockedInput.helpText)
+  })
+  it('should render disabled when disabled is passed', () => {
+    const { sut } = makeSut()
+    const input = sut.getByTestId('input') as HTMLInputElement
+    expect(input.disabled).toBe(true)
+  })
+})
