9f0a0ebaecfbc65b08f44e7f6bae6c705df30eaf
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-29 16:52:35 -0300

    style: add colors

---
+++ src/presentation/styles/colors.tsx
@@ -0,0 +1,14 @@
+type Color = {
+  [prop in number | string]: string;
+}
+
+export const grey: Color = {
+  0: '#fafafa',
+  50: '#f5f5f5',
+  100: '#eeeeee',
+  200: '#e0e0e0',
+  600: '#7a7a7a',
+  700: '#616161',
+  A50: '#dbdbdb',
+  A100: '#d5d5d5'
+}
