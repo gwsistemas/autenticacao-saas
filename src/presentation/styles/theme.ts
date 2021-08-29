683eac9725d0b1ea7d677c5912c20e4af79601fa
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-08-29 11:38:06 -0300

    feat: theme defaul creation

---
+++ src/presentation/styles/theme.ts
@@ -0,0 +1,49 @@
+import { DefaultTheme } from 'styled-components'
+
+export const defaultTheme: DefaultTheme = {
+  borderRadius: '',
+  colors: {
+    common: {
+      black: '#000',
+      white: '#000'
+    },
+    primary: {
+      light: '#3273dc',
+      main: '#00287c',
+      contrastText: '#fff'
+    },
+    error: {
+      main: 'red'
+    }
+  },
+  typography: {
+    fontFamily: "'Nanum Gothic', sans-serif",
+    htmlFontSize: 16,
+    fontSize: 14,
+    fontWeight: 400,
+    h1: {
+      fontFamily: "'Nanum Gothic', sans-serif",
+      fontSize: '4rem',
+      fontWeight: 400,
+      lineHeight: 1.2
+    },
+    body1: {
+      fontFamily: "'Nanum Gothic', sans-serif",
+      fontSize: '4rem',
+      fontWeight: 400,
+      lineHeight: 1.5
+    }
+  },
+  shadows: [
+    'none',
+    '0 1px 2px rgb(10 10 10 / 10%)'
+  ],
+  text: {
+    primary: '#363636',
+    secondary: '#222',
+    disabled: '#7a7a7a'
+  },
+  background: {
+    default: '#fff'
+  }
+}
