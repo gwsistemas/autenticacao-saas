ef8efe2d14cd1f48816d4a35f8268802b0155413
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-16 19:00:07 -0300

    feat: add other path to login

--- src/main/router/router.tsx
+++ src/main/router/router.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Home, Login } from '@/presentation/pages'
+import { Home } from '@/presentation/pages'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const Router: React.FC = () => {
+type Props = {
+  makeLogin: React.FC
+}
+
+const Router: React.FC<Props> = ({ makeLogin }: Props) => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path={['/', '/login']} component={makeLogin} />
         <Route path="/home" component={Home} />
       </Switch>
     </BrowserRouter>
