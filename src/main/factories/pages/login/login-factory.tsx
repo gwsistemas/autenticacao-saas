ad53b2ad3141684e90dcd59982736fdab5d00009
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-22 01:02:00 -0300

    feat: add localCache

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/usecases/remote-authentication-factory'
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
+import { makeLocalCookieCacheStorage } from '../../cache/cookie-storage-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       validation={makeLoginValidation()}
       authentication={makeRemoteAuthentication()}
+      localCache={makeLocalCookieCacheStorage()}
     />
   )
 }
