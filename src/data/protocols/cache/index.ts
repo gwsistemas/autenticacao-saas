0287daf94ee8852d9d8914b45ee6460da738e130
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-28 13:06:27 -0300

    refactor: remove code unused

--- src/data/protocols/cache/index.ts
+++ src/data/protocols/cache/index.ts
@@ -1,13 +1,3 @@
-export interface CacheStorage<T = string, S = any, R = any> {
-  get: (key: T) => R
-  set: (key: T, value: S) => void
-}
-
-export interface CacheClient<R> {
-  get: (key: string) => R
-  set: (key: string, value: any) => void
-}
-
 export interface SetStorage {
   set: (key: string, value: object) => void
 }
