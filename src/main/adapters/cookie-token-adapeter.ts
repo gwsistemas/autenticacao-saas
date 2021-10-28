1b38b60cbcbe7befc711a52d2f875a48b8957070
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-28 13:05:12 -0300

    refactor: remove cache-storage and cookies

--- src/main/adapters/cookie-token-adapeter.ts
+++
@@ -1,5 +0,0 @@
-import { makeLocalCookieCacheStorage } from '../factories/cache/cookie-storage-factory'
-
-export const getCurrentTokenAdapter = (): string | null => {
-  return makeLocalCookieCacheStorage().get('client_token')
-}
