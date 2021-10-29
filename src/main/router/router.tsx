105d6aec51147ca65a387fcc9dc04ec45ec78c77
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2021-10-29 01:24:22 -0300

    refactor: use PrivateRoute

--- src/main/router/router.tsx
+++ src/main/router/router.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { makeHome } from '@/main/factories/pages/home/home-factory'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
+import { PrivateRoute } from '@/main/proxies'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path={['/', '/login']} component={makeLogin} />
-        <Route path="/home" component={makeHome} />
+        <PrivateRoute path="/home" component={makeHome} />
       </Switch>
     </BrowserRouter>
   )
